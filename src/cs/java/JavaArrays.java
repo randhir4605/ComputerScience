ecef4d25638e05059060c76bb36b010b9198ea86
Randhir Raj <randhir4605@gmail.com>
2022-01-26 22:55:47 +0530

    Create JavaArrays.java

---
+++ src/cs/java/JavaArrays.java
@@ -0,0 +1,11 @@
+package cs.java;
+
+import java.util.Arrays;
+
+public class JavaArrays {
+	public static void main(String[] args) {
+		char[] arr1= {'X','Y'};
+		char[] arr2= {'Y','X'};
+		System.out.println(Arrays.compare(arr1, arr2));
+	}
+}
